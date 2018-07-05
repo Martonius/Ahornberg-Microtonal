702f45222d74675366b0c1d99019598aa8149106
Marty Carlton <martycarlton@ymail.com>
2018-07-05 09:46:52 -0700

    Updated Ahornberg.hpp

--- src/Ahornberg.hpp
+++ src/Ahornberg.hpp
@@ -6,10 +6,10 @@ using namespace rack;
 
 extern Plugin *plugin;
 
+//added/revised from Template.hpp example
+// Forward-declare each Model, defined in each module source file
+extern Model *modelEqualDivision;
+
 ////////////////////
 // module widgets
 ////////////////////
-
-struct EqualDivisionWidget : ModuleWidget {
-	EqualDivisionWidget();
-};
